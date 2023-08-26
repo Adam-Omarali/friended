92cc704cb65128baa60141e65faa9231b0271c17
jonnyACCI <jshan9078@gmail.com>
2023-08-26 19:03:18 -0400

    restored some files

--- app/profile/profileComponent.tsx
+++ app/profile/profileComponent.tsx
@@ -5,10 +5,39 @@ import Link from 'next/link';
 
 const ProfileComponent = () => {
 
+    const [socialChoice, setSocialChoice] = useState();
+    const [socialChoiceMessage, setSocialChoiceMessage] = useState('select social.');
+    const [iconChoice,setIconChoice] = useState('https://static.thenounproject.com/png/3012911-200.png');
+    const [dropDisplay,setDropDisplay] = useState(false);
+
+    function handleChoices(){
+        setDropDisplay(true);
+    }
+
+    const handleSelection = (value: any) => {
+        setIconChoice(value.icon);
+        setSocialChoice(value.name);
+        setDropDisplay(false);
+        setSocialChoiceMessage(value.placeholder);
+    }
+
     const skills=['Javascript','React','Node.js','Tailwind CSS'];
   const interests=['Web Development','UI/UX Design','Blockchain','Data Science']
   const experiences=['Frontend Developer at Ignition (3 years)','Software Engineer at Ignition (2 years)'];
-  
+
+  const socialMediaOptions = [
+    {
+        name:"Instagram",
+        icon:"https://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c521.png",
+        placeholder:"enter ig handle."
+    },
+    {
+        name:"Discord",
+        icon:"https://www.freepnglogos.com/uploads/discord-logo-png/concours-discord-cartes-voeux-fortnite-france-6.png",
+        placeholder:"enter discord handle."
+    }
+]
+
   return (
     <body className="">
       <div className="">
@@ -56,6 +85,21 @@ const ProfileComponent = () => {
             <div className="flex relative w-500px h-48px group justify-center items-center z-1001 mt-7">
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-2xl cursor-pointer" htmlFor="file_input" >change pfp.</label>
                 <input className="drop-shadow-2xl bg-lightgray placeholder-lightpurple text-sm font-bold shadow appearance-none border rounded-2xl w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline" id="file_input" type="file" placeholder="change pfp"/>
+            </div>
+            <div className="flex relative w-500px h-48px group justify-center items-center z-1001 mt-7">
+                <input className="drop-shadow-2xl bg-lightgray placeholder-lightpurple text-xl font-bold shadow appearance-none border rounded-2xl w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"  type="text" placeholder={socialChoiceMessage}/>
+                <span className="cursor-pointer flex absolute right-0 bg-transparent rounded text-base text-gray-600 p-2" >
+                  <img onClick = {handleChoices} src={iconChoice} alt="linkedinlogo" width={25} height={25}/>
+                </span>
+            </div>
+            <div className=" relative w-500px h-48px group justify-center items-center z-1001 mt-7" id="resultsList">{dropDisplay ? (
+                    socialMediaOptions.map((result, id)=>{
+                        return <div id="searchResult" key={id} onClick={()=>{handleSelection(result)}}>
+                          {result.name}
+                        </div>  
+                    })           
+
+                ) : (<h1></h1>)}
             </div>
             <div className="flex relative w-500px h-48px group justify-center items-center z-1001 mt-7">
               <button className="text-lightpink font-black text-3xl font-poppins bg-white rounded-2xl  py-2 px-3 leading-tight">save.</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
+      
       colors: {
+        'lightpurple': '#947795',
+        'lightgray': '#EFECEC',
+        'lightpink': '#DA9CDF',
+        'peach': '#DFCD9C',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
         btn: {
