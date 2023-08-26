928c080cda302e44bc05ee64652756737d2e87eb
jonnyACCI <jshan9078@gmail.com>
2023-08-26 18:39:56 -0400

    Added socials options and client-side components

--- app/view/page.tsx
+++ app/view/page.tsx
@@ -17,8 +17,8 @@ export default async function View() {
       <div className="">
         <h1 className="text-white body-font font-poppins text-5xl font-black top-7 mb-5 absolute left-0 top-0 w-16 h-16 ml-10" >friended.</h1>
         <div id="topRight" className="flex absolute top-5  w-16 h-16 ml-10">
-          <button className=" text-lightpink font-black text-3xl font-poppins bg-white rounded-2xl  py-2 px-3 leading-tight">connect.</button>
-          <button  className=" text-lightpink font-black text-3xl font-poppins bg-white rounded-2xl  ml-5  py-2 px-3 leading-tight">profile.</button>
+          <button className=" text-lightpink font-black text-3xl font-poppins bg-white rounded-2xl  py-2 px-3 leading-tight"><Link href="/correlation">connect.</Link></button>
+          <button  className=" text-lightpink font-black text-3xl font-poppins bg-white rounded-2xl  ml-5  py-2 px-3 leading-tight"><Link href="/profile">profile</Link></button>
           <img className=" w-50 h-50 ml-5 rounded-full " src="https://wallpapers.com/images/featured/minimalist-7xpryajznty61ra3.jpg" alt="Rounded avatar"></img>
         </div>
       </div>
