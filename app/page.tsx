793bb2d1f308df9a2a2472a53dc71735b0d0c386
Adam <adamomarali37@gmail.com>
2023-08-27 09:38:44 -0400

    improve front page

--- app/page.tsx
+++ app/page.tsx
@@ -14,25 +14,22 @@ export default function Index() {
   //   return <div>{session.user?.name}</div>;
   // }
   return (
-    <div>
-      {/* Rest of your component */}
-      <body className="w-full flex-center min-h-screen">
-        <div className="flex-center">
-          <div className="ml-[10vw]">
-            <h1 className="product-name">friended.</h1>
-            <h2 className="product-description">
-              Connect with hackers <br />
-              and fellow enthusiasts.
-            </h2>
-            <Button id="customButton" className="text-lightpink font-black text-3xl font-poppins bg-white rounded-2xl  py-2 px-3 leading-tight mt-5"  onClick={() => signIn("google")}>login with google.</Button>
-          </div>
-          <Image
-            src={landingImg}
-            alt="Image"
-            className="landing-image"
-          />
-        </div>
-      </body>
+    <div className="w-full flex min-h-screen items-center justify-center">
+      <div className="ml-[10vw]">
+        <h1 className="product-name">friended.</h1>
+        <h2 className="product-description">
+          Connect with hackers <br />
+          and fellow enthusiasts.
+        </h2>
+        <Button
+          id="customButton"
+          className="text-lightpink font-black text-3xl font-poppins bg-white rounded-2xl  py-2 px-3 leading-tight mt-5"
+          onClick={() => signIn("google")}
+        >
+          login with google.
+        </Button>
+      </div>
+      <Image src={landingImg} alt="Image" />
     </div>
   );
 }
